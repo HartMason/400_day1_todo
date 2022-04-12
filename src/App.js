79ebebeaf6ac8e71fe6d9c437df9f9146230eb99
HartMason <hartmason85@gmail.com>
2022-04-12 15:56:01 -0500

    second commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,87 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isClicked: false,
+      todos: [
+        {id: '1', todo: 'Wash Car'},
+        {id: '1', todo: 'Clean Room'}  
+      ],
+      text: ''
+    }
+  }
+
+
+componentDidMount() {
+  console.log("Mounted")
+  console.log(this.state.todos)
 }
+
+ componentDidUpdate() {
+  console.log("updated", this.state.todos)
+}
+
+handleClick = (event) => {
+  const todo = this.state.todo;
+  console.log(todo)
+  this.setState({
+    todos: [... this.state.todos, { id: this.state.todos.length + 1, todo }],
+    todo: "",
+    isClicked: !this.state.isClicked     
+  })
+  console.log(this.state.isClicked)
+}
+
+handleChange = (event) => {
+  this.setState({
+    todo: event.target.value,
+  });
+  console.log(this.state.todo);
+}
+
+handleDelete = (event, index) => {
+  console.log(index)
+  let objectCopy = [...this.state.todos]
+  objectCopy.splice(index, 1)
+  this.setState({todos: [...objectCopy]})
+};
+
+
+render() {
+  return <div className = "app">
+    <section>
+      <label htmlFor="todos">ToDos</label>
+      <input type="todos"
+        onChange={this.handleChange}
+        value={this.state.todo}
+      />
+      <button onClick={this.handleClick}>Submit</button>
+      <ul>
+        {this.state.todos.map((todo, index) => {
+        return (
+          <div>
+            <li key={todo.id}>{todo.todo}</li>
+            <button onClick={(evt) => this.handleDelete(evt, index)}>Delete</button>
+          </div>
+        )
+        })}
+      </ul>
+    </section>
+  </div>
+}
+
+
+
+
+
+
+
+
+
+}; 
 
 export default App;
